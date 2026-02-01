8f0d6c69bac0bfd30e7d56ed1a197e1029ce3552
osgilliath <shankhdhar.madhav@gmail.com>
2026-02-01 23:31:42 +0530

    centre alignment

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+const blessed = require('blessed');
 const { renderDigit, renderColon } = require('./digital-clock.js');
 
-function displayTime() {
-  console.clear();
+// Create a screen object.
+const screen = blessed.screen({
+  smartCSR: true,
+  title: 'Lazy Clock'
+});
+
+// Create a box to hold the clock.
+const clockBox = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: 'shrink',
+  height: 'shrink',
+  content: '',
+  tags: true,
+  style: {
+    fg: 'white',
+    bg: 'black'
+  }
+});
+
+// Append our box to the screen.
+screen.append(clockBox);
+
+function updateClock() {
   const now = new Date();
   const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
   const date = now.toLocaleDateString();
@@ -21,29 +44,25 @@ function displayTime() {
       `${hour1.split('\n')[i]} ${hour2.split('\n')[i]} ${colon.split('\n')[i]} ${minute1.split('\n')[i]} ${minute2.split('\n')[i]} ${colon.split('\n')[i]} ${second1.split('\n')[i]} ${second2.split('\n')[i]}`
     );
   }
-
+  
   const clockWidth = output[0].length;
-  const clockHeight = output.length + 2; // +2 for the date and the empty line
-
-  const termWidth = process.stdout.columns || 80;
-  const termHeight = process.stdout.rows || 24;
-
-  const horizontalPadding = Math.floor((termWidth - clockWidth) / 2);
-  const verticalPadding = Math.floor((termHeight - clockHeight) / 2);
-
-  const horizontalPaddingStr = ' '.repeat(horizontalPadding);
-
-  for (let i = 0; i < verticalPadding; i++) {
-    console.log('');
-  }
-
-  for (const line of output) {
-    console.log(horizontalPaddingStr + line);
-  }
-
   const datePadding = Math.floor((clockWidth - date.length) / 2);
-  const centeredDate = ' '.repeat(datePadding) + date;
-  console.log(horizontalPaddingStr + '\n' + horizontalPaddingStr + centeredDate);
+  const centeredDate = ' '.repeat(datePadding > 0 ? datePadding : 0) + date;
+
+  clockBox.setContent(output.join('\n') + '\n' + centeredDate);
+  screen.render();
 }
 
-setInterval(displayTime, 1000);
+// Initial display
+updateClock();
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+// Render the screen.
+screen.render();
